29ed5da0fdf2fde0fc70b1ae9ee639d66b0fb143
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2022-03-02 17:27:51 +0100

    added functions to compute accuracy and test metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,19 @@
 import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
-def compute_accuracy(pred_label, true_label):
-    pred_label = pred_label.astype(np.int32)
-    true_label = true_label.astype(np.int32)
+def compute_accuracy(true_labels, pred_labels):
+    pred_labels = pred_labels.astype(np.int32)
+    true_labels = true_labels.astype(np.int32)
 
-    same = (pred_label == true_label).sum()
-    acc = same / len(pred_label)
+    acc = accuracy_score(true_labels, pred_labels)
     return acc
+
+def compute_test_metrics(pred_labels, true_labels):
+    true_labels = true_labels.astype(np.int32)
+    pred_labels = pred_labels.astype(np.int32)
+
+    acc = accuracy_score(true_labels, pred_labels)
+    cm = confusion_matrix(true_labels, pred_labels)
+    f1 = f1_score(true_labels, pred_labels)
+
+    return acc, cm, f1
